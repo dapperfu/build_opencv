f22eac0b11663caddb6799c8e7b6ddf508ab8c2f
jed <jed+build_opencv@g5-LINUX>
2019-04-05 13:35:53 -0400

    .

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-HERE=`pwd`
 
-mkdir ${HERE}/build
-cd ${HERE}/build
+mkdir ${WORKSPACE}/build
+cd ${WORKSPACE}/build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D INSTALL_PYTHON_EXAMPLES=ON \
 	-D INSTALL_C_EXAMPLES=OFF \
 	-D OPENCV_ENABLE_NONFREE=ON \
-	-D OPENCV_EXTRA_MODULES_PATH=${HERE}/opencv_contrib/modules \
-	-D PYTHON_DEFAULT_EXECUTABLE=${HERE}/bin/python \
-	-D PYTHON_EXECUTABLE=${HERE}/bin/python \
+	-D OPENCV_EXTRA_MODULES_PATH=${WORKSPACE}/opencv_contrib/modules \
+	-D PYTHON_DEFAULT_EXECUTABLE=${WORKSPACE}/bin/python \
+	-D PYTHON_EXECUTABLE=${WORKSPACE}/bin/python \
 	-D PYTHON2_EXECUTABLE=`which python2` \
-	-D BUILD_EXAMPLES=ON ${HERE}/opencv
+	-D BUILD_EXAMPLES=ON ${WORKSPACE}/opencv
 
 make -j6
