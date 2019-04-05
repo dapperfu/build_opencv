bd09cd38a3d3fcbd99603cccabeadca7fc4396bc
jed <jed+build_opencv@g5-LINUX>
2019-04-05 19:51:30 -0400

    .

--- 01_opencv_checkout.sh
+++ 01_opencv_checkout.sh
@@ -2,7 +2,11 @@
 
 OPENCV_VER=${OPENCV_VER:-4.0.1}
 
+if [ ! -d "opencv" ] ; then
 git clone --recurse-submodules --branch=${OPENCV_VER} --depth=1 https://github.com/opencv/opencv.git
-git clone --recurse-submodules --branch=${OPENCV_VER} --depth=1 https://github.com/opencv/opencv_contrib.git
+fi
 
+if [ ! -d "opencv_contrib" ] ; then
+git clone --recurse-submodules --branch=${OPENCV_VER} --depth=1 https://github.com/opencv/opencv_contrib.git
+fi
 
