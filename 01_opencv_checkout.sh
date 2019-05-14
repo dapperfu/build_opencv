62b24968653e3ad693e700a8db890f7c1c456177
jed <jed@m6700.lan>
2019-05-14 03:22:49 -0400

    .

--- 01_opencv_checkout.sh
+++ 01_opencv_checkout.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-OPENCV_VER=${OPENCV_VER:-4.0.1}
+OPENCV_VER=${OPENCV_VER:-4.1.0}
 
 if [ ! -d "opencv" ] ; then
 git clone --recurse-submodules --branch=${OPENCV_VER} --depth=1 https://github.com/opencv/opencv.git
